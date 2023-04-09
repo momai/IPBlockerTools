6ad2a029b12e0f4ea5d8b2e3931b7b16efade8e7
momai <mamai2010@gmail.com>
2023-04-10 00:36:39 +0300

    Update state_ip.py

--- state_ip.py
+++ state_ip.py
@@ -1,61 +1,59 @@
-#!/bin/bash
-source .env
-
-function get_abuse_ipdb_info() {
-    local ip_address="$1"
-    local api_key=$ABUSEIPDB_API
-    local whois_info=$(whois "$ip_address")
-#    local whois_info=$(whois "$ip_address" | grep -E 'inetnum:|netname:|descr:|country:|admin-c:|tech-c:|status:|mnt-by:|created:|last-modified:|source:')
-    local abuse_info=$(curl -s -G https://api.abuseipdb.com/api/v2/check \
-        --data-urlencode "ipAddress=$ip_address" \
-        -d maxAgeInDays=90 \
-        -d verbose \
-        -H "Key: $api_key" \
-        -H "Accept: application/json" | jq -r '.data')
-
-    echo "Whois информация:"
-    echo "$whois_info"
-    echo ""
-    echo "AbuseIPDB информация:"
-    echo "$abuse_info"
-}
-
-# Создайте цепочку BLOCKED_IPS, если она еще не существует
-sudo iptables -N BLOCKED_IPS 2>/dev/null || true
-
-# Прочитайте список IP-адресов из файла blocked_ips.txt и блокируйте их в iptables
-#while read -r line; do
-#    IP_ADDRESS=$(echo "$line" | awk '{print $1}')
-#    duplicate_count=$(echo "$line" | awk '{print $2}')
-
-#    read -p "Вы хотите заблокировать IP-адрес $IP_ADDRESS (дублей: $duplicate_count)? (y/n) " answer </dev/tty
-
-#    if [ "$answer" == "y" ]; then
-#        sudo iptables -A BLOCKED_IPS -s "$IP_ADDRESS" -j DROP
-#        echo "Заблокирован IP-адрес $IP_ADDRESS"
-#    else
-#        echo "Пропуск IP-адреса $IP_ADDRESS"
-#    fi
-#done < blocked_ips.txt
-
-while read -r line; do
-    IP_ADDRESS=$(echo "$line" | awk '{print $1}')
-    duplicate_count=$(echo "$line" | awk '{print $2}')
-
-    echo "Информация для IP-адреса $IP_ADDRESS (дублей: $duplicate_count):"
-    get_abuse_ipdb_info "$IP_ADDRESS"
-    echo ""
-
-    read -p "Вы хотите заблокировать IP-адрес $IP_ADDRESS (дублей: $duplicate_count)? (y/n) " answer </dev/tty
-
-    if [ "$answer" == "y" ]; then
-        sudo iptables -A BLOCKED_IPS -s "$IP_ADDRESS" -j DROP
-        echo "Заблокирован IP-адрес $IP_ADDRESS"
-    else
-        echo "Пропуск IP-адреса $IP_ADDRESS"
-    fi
-done < blocked_ips.txt
+import re
+from collections import Counter
+from datetime import datetime, timedelta
+from io import SEEK_END, SEEK_CUR
+import json
+import subprocess
 
 
-# Примените цепочку BLOCKED_IPS к INPUT, если она еще не применена
-sudo iptables -C INPUT -j BLOCKED_IPS 2>/dev/null || sudo iptables -A INPUT -j BLOCKED_IPS
+# Чтение JSON-конфига
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+ip_count = Counter()
+
+# Шаблон регулярного выражения для IP-адресов и времени
+ip_pattern = r'(?P<ip>\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b).*\[(?P<date>\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2})'
+ip_regex = re.compile(ip_pattern)
+
+# Временной интервал для анализа (10 секунд)
+time_interval = timedelta(seconds=config["time_interval_seconds"])
+
+# Текущее время
+now = datetime.utcnow()
+
+def readlines_reverse(file):
+    file.seek(0, SEEK_END)
+    position = file.tell()
+    line = ''
+    while position >= 0:
+        file.seek(position)
+        next_char = file.read(1)
+        if next_char == '\n':
+            yield line[::-1]
+            line = ''
+        else:
+            line += next_char
+        position -= 1
+    yield line[::-1]
+
+# Обработка лог-файла с конца
+with open(config["file_path"], 'r') as file:
+    for line in readlines_reverse(file):
+        match = ip_regex.search(line)
+        if match:
+            log_time = datetime.strptime(match.group('date'), "%d/%b/%Y:%H:%M:%S")
+            if (now - log_time).total_seconds() <= time_interval.total_seconds():
+                ip_count.update([match.group('ip')])
+            else:
+                break
+
+# Вывод результатов в консоль и в файл
+with open(config["output_file_path"], 'w') as f:
+    for ip, count in ip_count.most_common():
+        if count > config["output_threshold_count"]:
+            print(f'{ip} {count}')
+            f.write(f'{ip} {count}\n')
+
+# Выполнение скрипта на языке shell
+subprocess.call(['/bin/bash', 'block_ips.sh'])
